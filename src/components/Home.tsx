cc1fe213b6f540d75202cf41fd1f10cea0963079
remyli <rapi.remyli@gmail.com>
2019-11-15 19:05:48 +0900

    temp

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,6 +11,12 @@ import {
 } from "@material-ui/system";
 import ThreadLink from "./links/ThreadLink";
 
+import List from "@material-ui/core/List";
+import ListItem, { ListItemProps } from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import NotesIcon from "@material-ui/icons/Notes";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+
 const Box = styled.div<PaletteProps & SpacingProps & TypographyProps>`
   ${palette}
   ${spacing}
@@ -24,19 +30,27 @@ export const Home: React.FC<HomeProps> = (props: HomeProps) => {
   return (
     <Box>
       <p>Home</p>
-      {props.threads && props.threads.length > 0 ? (
-        props.threads.map(thread => {
-          return (
-            <ul key={`thread-${thread.id}`}>
-              <li>
-                <ThreadLink thread_id={thread.id}>- {thread.title}</ThreadLink>
-              </li>
-            </ul>
-          );
-        })
-      ) : (
-        <div>まだスレッドはない</div>
-      )}
+      <List component="nav" aria-label="main mailbox folder">
+        {props.threads && props.threads.length > 0 ? (
+          props.threads.map(thread => {
+            return (
+              <ListItem
+                button
+                key={`thread-${thread.id}`}
+                component="a"
+                href={`/thread/${thread.id}`}
+              >
+                <ListItemIcon tabIndex={-1}>
+                  <NotesIcon />
+                </ListItemIcon>
+                <ListItemText primary={thread.title} />
+              </ListItem>
+            );
+          })
+        ) : (
+          <div>まだスレッドはない</div>
+        )}
+      </List>
     </Box>
   );
 };
